f0c63e768bd771c60dc00be4d2f3607afd66fa47
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-05-18 19:33:40 +0500

    Create app.js

---
+++ framework/framework/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  'value': true
+});
+exports.reagir = undefined;
+async function reagir(_0xa780dd, _0x315b13, _0x16e734, _0x23d190) {
+  await _0x315b13.sendMessage(_0xa780dd, {
+    'react': {
+      'text': _0x23d190,
+      'key': _0x16e734.key
+    }
+  });
+}
+exports.reagir = reagir;
