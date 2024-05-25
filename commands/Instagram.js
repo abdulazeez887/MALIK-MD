0e57912c9eb549b29c64328839df48d0a569b30a
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-05-25 16:38:55 +0500

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,21 +1,15 @@
 const {cmd , Config , prefix,getBuffer,tlang,fetchJson } = require('../lib')
  const fetch = require('node-fetch')
 
-
- let cap = `╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${Config.botname}`
- cmd({
-         pattern: "insta",
-         alias: ["ig"],
-         desc: "download instagram videos",
-         category: "downloader",
-         use: "paste insta video link"
-     },
-     async(Void,citel,text) => {
- if(!text) return citel.reply('Give me insta video link')
- let txt = text ? text.split(" ")[0]:'';
- if (!/instagram/.test(txt)) return await citel.reply(`Please give me valid instagram video link..!`);
- let data;
- try{ data = await (await fetch(`https://vihangayt.me/download/instagram2?url=${text}`)).json();} 
- catch { return citel.reply(`An error occurred`);  }
- return Void.sendMessage(citel.chat, {video : {url : data.result[0].url },caption: cap,width: 600,height: 490, },{ quoted: citel })
+cmd({ pattern: "insta", alias: ["ig"], desc: "download instagram videos", category: "downloader", use: "paste insta video link" }, async(Void,citel,text) => {
+  if(!text) return citel.reply('Give me insta video link')
+  let txt = text ? text.split(" ")[0]:'';
+  if (!/instagram/.test(txt)) return await citel.reply(`Please give me valid instagram video link..!`);
+  let data;
+  try{
+    data = await fetchJson(`(https://api.maher-zubair.tech/download/instagram?url=));
+  } catch (err) {
+    return citel.reply(`An error occurred: ${err.message}`);
+  }
+  return Void.sendMessage(citel.chat, {video : {url : data.result[0].url },caption: cap,width: 600,height: 490, },{ quoted: citel })
 })
